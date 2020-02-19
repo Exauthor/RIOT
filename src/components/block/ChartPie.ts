8d1bf9e9f2ead5a6ddc622db7f215fca61b87278
Warinyourself <dmitriysuvorov1997@gmail.com>
2020-02-20 00:50:23 +0300

    Modify ChartPie on storage logic

--- src/components/block/ChartPie.ts
+++ src/components/block/ChartPie.ts
@@ -1,6 +1,7 @@
 import { Component, Prop, Vue } from 'vue-property-decorator'
 import { IChartPieSetting } from '@/types'
 import { CreateElement, VNode } from 'vue/types'
+import { SystemModule } from '@/store/modules/system'
 import * as d3 from 'd3'
 
 @Component({
@@ -10,8 +11,31 @@ export default class ChartPie extends Vue {
   @Prop({ default: 'temperature', type: String }) option!: string
   @Prop({ type: Object, required: true }) settings!: IChartPieSetting
 
-  get percent():number {
-    return this.settings.percent
+  get infoBlock() {
+    if (typeof this.settings.percent === 'object') {
+      return SystemModule.getSystemInfoBlock(this.settings.percent.id)
+    }
+    return false
+  }
+
+  get percent() {
+    const percent = this.settings.percent
+    if (typeof percent === 'object') {
+      const { maxValue, minValue } = this.infoBlock
+      const onePercent = ((maxValue || 100) - (minValue || 0)) / 100
+      return this.infoBlock.value / onePercent
+    } else {
+      return percent
+    }
+  }
+
+  get value() {
+    const value = this.settings.value
+    if (typeof value === 'object') {
+      return this.infoBlock && this.infoBlock.value
+    } else {
+      return value
+    }
   }
 
   render(h: CreateElement): VNode {
@@ -23,11 +47,11 @@ export default class ChartPie extends Vue {
   }
 
   async initPieChart() {
-    let that = this
-    let margin = 10
-    let minValue = this.$refs.svgPie.parentElement.offsetWidth
-    let radius = (minValue / 2) - margin * 2
-    let range = d3.scaleLinear().domain([0, 100]).range([17, 100])
+    const that = this
+    const margin = 10
+    const minValue = this.$refs.svgPie.parentElement.offsetWidth
+    const radius = (minValue / 2) - margin * 2
+    const range = d3.scaleLinear().domain([0, 100]).range([17, 100])
     const node = this.$refs.svgPie
     let svg = d3.select(node as Element)
       .attr('width', minValue)
@@ -35,34 +59,35 @@ export default class ChartPie extends Vue {
       .append('g')
       .attr('transform', `translate(${minValue / 2}, ${minValue / 2})`)
 
-    let group = svg.append('g').attr('style', 'transform: rotate(150deg)')
+    const group = svg.append('g').attr('style', 'transform: rotate(150deg)')
 
     let oldValue = 0
 
     let arc = d3.arc()
       .outerRadius(radius)
-      .innerRadius(radius * 0.9)
+      .innerRadius(radius * 0.85)
       .startAngle(1)
       .cornerRadius(radius * 0.1)
 
     group.append('path')
-      .attr('d', arc.endAngle(Math.PI / 2))
-      .attr('style', 'fill: rgba(0,0,0,.5)')
+      .attr('d', arc({ endAngle: Math.PI * 2 }))
+      .attr('style', 'fill: var(--color-bg)')
 
     let path = group.append('path')
       .datum({ endAngle: 1 })
-      .attr('d', arc({ endAngle: (range(that.percent) / 100) * (2 * Math.PI) }))
+      .attr('d', arc({ endAngle: (range(this.percent) / 100) * (2 * Math.PI) }))
 
     let titleText = svg.append('text')
       .attr('text-anchor', 'middle')
       .attr('dy', -20)
       .attr('dx', 0)
 
-    let textValue = this.settings.value || that.percent || 0
+    let textValue = this.value || this.percent || 0
+    const pre = this.settings.pre || 'C°'
 
     let text = svg.append('text')
       .datum(textValue)
-      .text((d: any) => d + '%')
+      .text((d: any) => d + pre)
       .attr('class', 'middleText')
       .attr('text-anchor', 'middle')
       .attr('dy', 20)
@@ -73,25 +98,20 @@ export default class ChartPie extends Vue {
     path.attr('style', `fill: ${color}`)
 
     titleText.text(title + ':')
-      .attr('style', `fill: ${color}; font-size: 1rem`)
+      .attr('style', `fill: ${color}; font-size: .8rem`)
 
-    text.attr('style', `fill: ${color}; font-size: 2rem`)
+    text.attr('style', `fill: ${color}; font-size: 1.5rem`)
 
-    let animation = (transition: any, percent: number, oldValue: number) => {
+    const animation = (transition: any, percent: number, oldValue: number) => {
       transition.attrTween('d', (d: any) => {
-        let value = this.settings.value || that.percent
-
-        let textValue = value
-
-        let newAngle = (range(that.percent) / 100) * (2 * Math.PI)
+        let newAngle = (range(percent) / 100) * (2 * Math.PI)
         let interpolate = d3.interpolate(d.endAngle, newAngle)
-        let interpolateCount = d3.interpolate(oldValue, textValue)
+        let interpolateCount = d3.interpolate(oldValue, that.value || percent)
 
         return (t: any) => {
           d.endAngle = interpolate(t)
 
           let pathForegroundCircle = arc(d)
-          let pre = this.settings.pre || 'C°'
 
           text.text(`+${Math.floor(interpolateCount(t))}${pre}`)
 
@@ -102,15 +122,14 @@ export default class ChartPie extends Vue {
 
     let animate = async function() {
       path.transition()
-        .duration(800)
+        .duration(1200)
         .ease(d3.easeLinear)
         .call(animation, that.percent, oldValue)
 
-      oldValue = that.settings.value || that.percent
-
-      setTimeout(await animate, 1200)
+      oldValue = that.value || that.percent
     }
 
-    await animate()
+    animate()
+    setInterval(animate, 2000)
   }
 }
